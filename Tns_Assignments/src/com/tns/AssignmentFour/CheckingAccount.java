9f524a94a00a9a1f0e5469b6e8f126501819718e
alwindonald <alwindonald4004@gmail.com>
2024-11-07 21:53:38 +0530

    commit

--- Tns_Assignments/src/com/tns/AssignmentFour/CheckingAccount.java
+++ Tns_Assignments/src/com/tns/AssignmentFour/CheckingAccount.java
@@ -2,28 +2,5 @@ package com.tns.AssignmentFour;
 
 public class CheckingAccount extends Account{
 	
-
-	    public CheckingAccount(float balance) {
-	        super(balance);
-	    }
-
-	    @Override
-	    public void deposit(float amount) {
-	        balance += amount;
-	    }
-
-	    @Override
-	    public void withdraw(float amount) {
-	        if (balance >= amount) {
-	            balance -= amount;
-	        } else {
-	            System.out.println("Overdraft limit exceeded!");
-	        }
-	    }
-
-	    @Override
-	    public float getBalance() {
-	        return balance;
-	    }
 	}
 
